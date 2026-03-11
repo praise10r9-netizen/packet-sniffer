db672cc15aab1e4a6f26ddf6998a2fb68f2fc0a1
praise10r9-netizen <praise10r9@gmail.com>
2026-03-11 18:01:14 +0200

    capturing raw ethernet frame bytes..

--- src/main.c
+++ src/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
 
 int main()
 {
+ unsigned char buffer[65536];
+  int sock_raw;
+  sock_raw = socket(AF_PACKET,SOCK_RAW, htons(ETH_P_ALL));
+  
+  if(sock_raw < 0)
+  {
+    perror("Socket Error");
+    return 1;
+  }
+  printf("Packet sniffer started...\n");
+  while(1)
+  {
+    int data_size;
+    data_size = recvfrom(sock_raw, buffer, sizeof(buffer),0,NULL,NULL);
+    if(data_size < 0)
+    {
+      perror("Recvfrom error");
+      return 1;
+    }
+    printf("Packet captured: %d bytes\n",data_size);
+  }
   return 0;
 }
